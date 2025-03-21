7e6bb4d1946a70810c7bf3dd3a72e7b923fc5f6c
Dzmitry Drevich <jeidan2012@gmail.com>
2025-03-21 16:48:20 +0100

    feat: add sollution for tasks 1-4

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return Date.parse(date);
 }
 
 /**
@@ -31,8 +31,8 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  return new Date(date).toTimeString().slice(0, 8);
 }
 
 /**
@@ -46,8 +46,10 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  return new Intl.DateTimeFormat('en-GB', {
+    weekday: 'long',
+  }).format(new Date(date));
 }
 
 /**
@@ -61,8 +63,14 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const currentDate = new Date(date);
+  const currentWeekDay = currentDate.getDay();
+  const daysTillFriday = (5 - currentWeekDay + 7) % 7 || 7;
+  const nextFriday = new Date(
+    currentDate.setDate(currentDate.getDate() + daysTillFriday)
+  );
+  return nextFriday;
 }
 
 /**
