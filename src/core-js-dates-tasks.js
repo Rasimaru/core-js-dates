2a6774fc78fd6319f90c02594bff32dd000dd60e
Dzmitry Drevich <jeidan2012@gmail.com>
2025-03-23 10:29:09 +0100

    feat: add sollution for tasks 11, 13

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -219,8 +219,17 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const startDate = new Date(date);
+  let nextDate;
+  for (let month = startDate.getMonth(); month < 12; month += 1) {
+    const newDate = new Date(startDate.getFullYear(), month, 13);
+    if (newDate.getDay() === 5) {
+      nextDate = newDate;
+      break;
+    }
+  }
+  return new Date(nextDate);
 }
 
 /**
@@ -256,8 +265,30 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const startDate = new Date(period.start);
+  const endDate = new Date(period.end.split('-').reverse().join('-'));
+  const schedule = [];
+  while (startDate <= endDate) {
+    for (let i = 0; i < countWorkDays && startDate <= endDate; i += 1) {
+      schedule.push(
+        startDate
+          .toLocaleString('en-GB', {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+          })
+          .replaceAll('/', '-')
+      );
+      startDate.setDate(startDate.getDate() + 1);
+    }
+
+    for (let j = 0; j < countOffDays && startDate <= endDate; j += 1) {
+      startDate.setDate(startDate.getDate() + 1);
+    }
+  }
+
+  return schedule;
 }
 
 /**
